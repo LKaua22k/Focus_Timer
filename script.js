f02ae35aeca7b3a3c172d233734575f17de18cbf
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-30 08:25:56 -0300

    :tada:

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ const Play = {
     start:() => {
         Play.playButton.classList.add('hide')
         Play.pausebutton.classList.remove('hide')
+        Set.setButton.classList.add('hide')
+        Set.stopButton.classList.remove('hide')
+
     },
 
     close:() =>{
-      Play.pausebutton.classList.add('hide')
+        Play.pausebutton.classList.add('hide')
         Play.playButton.classList.remove('hide')
+    }
+}
+
+const Set = {
+    setButton: document.querySelector('.set'),
+    stopButton: document.querySelector('.stop'),
+
+    stop:() => {
+        Play.playButton.classList.remove('hide')
+        Play.pausebutton.classList.add('hide')
+        Set.setButton.classList.remove('hide')
+        Set.stopButton.classList.add('hide')
     }
 }
 
 Play.playButton.addEventListener('click', Play.start)
 Play.pausebutton.addEventListener('click', Play.close)
+Set.stopButton.addEventListener('click', Set.stop)
