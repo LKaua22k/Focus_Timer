7b70db8fc4ee145c42abc8eabae5f17c215fc407
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-30 16:16:16 -0300

    :tada:

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ const Play = {
             let seconds = Number(secondsDisplay.textContent)
             let minutes = Number(minutesDisplay.textContent)
 
+            if(minutes <= 0 && seconds <=0){
+                return;
+            }
+
             if(seconds <= 0){
                 seconds = 10
 
@@ -69,6 +73,10 @@ const Sound = {
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 let minutes
+function updateTimer(minutes,seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2,"0")
+    seconds.textContent = String(seconds).padStart(2,"0")
+}
 
 Play.playButton.addEventListener('click', Play.start)
 Play.pausebutton.addEventListener('click', Play.close)
