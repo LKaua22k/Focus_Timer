c19c46b0fb327192bb7c171eb19f1cb47f2e150f
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-06-30 10:04:54 -0300

    :tada:

--- script.js
+++ script.js
@@ -9,11 +9,25 @@ const Play = {
         Set.setButton.classList.add('hide')
         Set.stopButton.classList.remove('hide')
 
+        Play.countDown()
     },
 
     close:() =>{
         Play.pausebutton.classList.add('hide')
         Play.playButton.classList.remove('hide')
+    },
+
+    countDown:() => {
+        setTimeout(() => {
+            let seconds = Number(secondsDisplay.textContent)
+
+            if(seconds <= 0){
+                seconds = 60
+            }
+
+            secondsDisplay.textContent = seconds - 1
+            Play.countDown()
+        }, 1000)
     }
 }
 const Set = {
@@ -22,7 +36,7 @@ const Set = {
 
     start:() => {
         minutes = prompt("Quantos minutos?")
-
+        minutesDisplay.textContent = String(minutes).padStart(2,"0")
     },
 
     stop:() => {
@@ -47,6 +61,8 @@ const Sound = {
     }
 }
 
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 let minutes
 
 Play.playButton.addEventListener('click', Play.start)
